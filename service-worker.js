7d8e78168e03a1ebae2d7e7a5a53d6fe1c60f186
sukapisuka <tmpgamescom@gmail.com>
2025-12-11 18:06:52 +0200

    Bump service worker to v3 to force update for new Groups feature

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker for My Notes PWA
 // Enables offline functionality and caching
 
-const CACHE_NAME = 'my-notes-v2';
+const CACHE_NAME = 'my-notes-v3';
 const urlsToCache = [
     './',
     './index.html',
