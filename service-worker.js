e22a77fa324b1ce6f6b6d2516e46fbd12fba9ee5
sukapisuka <tmpgamescom@gmail.com>
2025-12-12 16:46:50 +0200

    UI Feature: Move 'New Note' button to floating action button (FAB) in bottom-right corner (bump v20)

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker for My Notes PWA
 // Enables offline functionality and caching
 
-const CACHE_NAME = 'my-notes-v19';
+const CACHE_NAME = 'my-notes-v20';
 const urlsToCache = [
     './',
     './index.html',
